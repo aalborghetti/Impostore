ec1b859fd5278df339a2cfeb19af1a0043870c5f
aalborghetti <alberto.alborghetti@gmail.com>
2026-03-01 14:46:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -241,11 +241,13 @@ function render() {
 
       <div class="row">
         <div class="label"><strong>Suggerimento per l’impostore</strong></div>
-        <div class="switch">
+        <div class="setting-controls">
+        <label class="switch">
           <input type="checkbox" id="hintToggle" ${app.impostorHintEnabled ? "checked" : ""}>
           <span class="slider"></span>
-        </div>
+        </label>
         <button class="info-btn" id="infoHint" aria-label="Info numero giocatori">ⓘ</button>
+        </div>
       </div>
 
       <div class="row">
